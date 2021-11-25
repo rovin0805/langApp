3b7c94d0e5963709e9cb3c8133befa3850fbe222
Choi Eun Jeong <qhdrn67@naver.com>
2021-11-26 00:31:02 +0900

    Manual Animations

--- App.js
+++ App.js
@@ -1,6 +1,40 @@
-import React from "react";
-import styled from "styled-components";
+import React, { useEffect, useState } from "react";
+import styled from "styled-components/native";
+
+const Container = styled.View`
+  flex: 1;
+  justify-content: center;
+  align-items: center;
+`;
+const Box = styled.TouchableOpacity`
+  background-color: tomato;
+  width: 200px;
+  height: 200px;
+`;
 
 export default function App() {
-  return null;
+  const [y, setY] = useState(0);
+  const [intervalId, setIntervalId] = useState(null);
+
+  const moveBox = () => {
+    const id = setInterval(() => setY((prev) => prev + 20), 10);
+    setIntervalId(id);
+  };
+
+  useEffect(() => {
+    if (y === 200) {
+      clearInterval(intervalId);
+    }
+  }, [y, intervalId]);
+
+  return (
+    <Container>
+      <Box
+        onPress={moveBox}
+        style={{
+          transform: [{ translateY: y }],
+        }}
+      />
+    </Container>
+  );
 }
