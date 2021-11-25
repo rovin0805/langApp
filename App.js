57929e934e74c505ffa4df0f94e6a6f1006a54a8
Choi Eun Jeong <qhdrn67@naver.com>
2021-11-26 01:05:03 +0900

    Animation and State with useRef

--- App.js
+++ App.js
@@ -1,5 +1,5 @@
-import React from "react";
-import { Animated, TouchableOpacity } from "react-native";
+import React, { useRef, useState } from "react";
+import { Animated, Easing, TouchableOpacity } from "react-native";
 import styled from "styled-components/native";
 
 const Container = styled.View`
@@ -18,14 +18,18 @@ const Box = styled.View`
 const AnimatedBox = Animated.createAnimatedComponent(Box);
 
 export default function App() {
-  const Y = new Animated.Value(0);
+  const [up, setUp] = useState(true);
+  const Y = useRef(new Animated.Value(0)).current; // maintaining current value from re-rendering
   // Y.addListener(() => console.log(Y));
+
+  const toggleUp = () => setUp((prev) => !prev);
 
   const moveBox = () => {
     Animated.timing(Y, {
-      toValue: 200,
+      toValue: up ? -200 : 200,
       useNativeDriver: true,
-    }).start();
+      easing: Easing.elastic(2),
+    }).start(toggleUp);
   };
 
   return (
