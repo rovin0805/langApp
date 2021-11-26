dd0530255c3b86215315cd9049bfbf21af8defa9
Choi Eun Jeong <qhdrn67@naver.com>
2021-11-26 18:02:04 +0900

    Card Project part Three

--- App.js
+++ App.js
@@ -10,6 +10,12 @@ const Container = styled.View`
   background-color: #00a8ff;
 `;
 
+const CardContainer = styled.View`
+  flex: 3;
+  justify-content: center;
+  align-items: center;
+`;
+
 const Card = styled(Animated.createAnimatedComponent(View))`
   background-color: white;
   width: 300px;
@@ -18,6 +24,16 @@ const Card = styled(Animated.createAnimatedComponent(View))`
   align-items: center;
   border-radius: 12px;
   box-shadow: 1px 1px 5px rgba(0, 0, 0, 0.2);
+  position: absolute;
+`;
+
+const BtnContainer = styled.View`
+  flex-direction: row;
+  flex: 1;
+`;
+
+const Btn = styled.TouchableOpacity`
+  margin: 0px 10px;
 `;
 
 export default function App() {
@@ -28,6 +44,11 @@ export default function App() {
     inputRange: [-250, 250],
     outputRange: ["-15deg", "15deg"],
     extrapolate: "extend",
+  });
+  const secondScale = positionX.interpolate({
+    inputRange: [-300, 0, 300],
+    outputRange: [1, 0.7, 1],
+    extrapolate: "clamp",
   });
 
   // Animations
@@ -41,14 +62,17 @@ export default function App() {
   });
   const goCenter = Animated.spring(positionX, {
     toValue: 0,
+    tension: 5,
     useNativeDriver: true,
   });
   const disappearLeft = Animated.spring(positionX, {
     toValue: -500,
+    tension: 5,
     useNativeDriver: true,
   });
   const disappearRight = Animated.spring(positionX, {
     toValue: 500,
+    tension: 5,
     useNativeDriver: true,
   });
 
@@ -61,9 +85,9 @@ export default function App() {
         positionX.setValue(dx);
       },
       onPanResponderRelease: (_, { dx }) => {
-        if (dx < -280) {
+        if (dx < -250) {
           disappearLeft.start();
-        } else if (dx > 280) {
+        } else if (dx > 250) {
           disappearRight.start();
         } else {
           Animated.parallel([onPressOut, goCenter]).start();
@@ -72,20 +96,46 @@ export default function App() {
     })
   ).current;
 
+  const closePress = () => {
+    disappearLeft.start();
+  };
+  const resetPress = () => {
+    goCenter.start();
+  };
+  const checkPress = () => {
+    disappearRight.start();
+  };
+
   return (
     <Container>
-      <Card
-        style={{
-          transform: [
-            { scale },
-            { translateX: positionX },
-            { rotateZ: rotation },
-          ],
-        }}
-        {...panResponder.panHandlers}
-      >
-        <Ionicons name="pizza" color="#192a56" size={98} />
-      </Card>
+      <CardContainer>
+        <Card style={{ transform: [{ scale: secondScale }] }}>
+          <Ionicons name="beer" color="#192a56" size={98} />
+        </Card>
+        <Card
+          style={{
+            transform: [
+              { scale },
+              { translateX: positionX },
+              { rotateZ: rotation },
+            ],
+          }}
+          {...panResponder.panHandlers}
+        >
+          <Ionicons name="pizza" color="#192a56" size={98} />
+        </Card>
+      </CardContainer>
+      <BtnContainer>
+        <Btn onPress={closePress}>
+          <Ionicons name="close-circle" color="white" size={58} />
+        </Btn>
+        <Btn onPress={resetPress}>
+          <Ionicons name="refresh-circle-outline" color="white" size={58} />
+        </Btn>
+        <Btn onPress={checkPress}>
+          <Ionicons name="checkmark-circle" color="white" size={58} />
+        </Btn>
+      </BtnContainer>
     </Container>
   );
 }
